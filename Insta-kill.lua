e9618657b87a3a22371f501840fbe98522ac423c
hadi <81989873+loadlua@users.noreply.github.com>
2024-08-13 17:06:28 -0400

    Create Insta-kill.lua

---
+++ Insta-kill.lua
@@ -0,0 +1,44 @@
+local animations = {
+    ["rbxassetid://12273188754"]=1.31,
+    ["rbxassetid://12296113986"]=1.2,
+}
+function ifind(t,a)
+    for i,v in pairs(t) do
+        if i==a then
+            return i
+        end
+    end
+    return false
+end
+local plr = game.Players.LocalPlayer
+getgenv().loop = coroutine.create(function()
+    local dothetech = false
+    local lastcf
+    while task.wait() do 
+        local character = plr.Character
+        local animate = character.Humanoid.Animator
+
+        for i,v in pairs(animate:GetPlayingAnimationTracks()) do
+            if ifind(animations, v.Animation.AnimationId) then
+                wait(animations[v.Animation.AnimationId])
+                dothetech=true
+                lastcf = character.HumanoidRootPart.CFrame
+                v.Stopped:Connect(function()
+                    dothetech=false
+                end)
+                repeat wait()
+                    workspace.Camera.CameraType = Enum.CameraType.Scriptable
+                    character.HumanoidRootPart.CFrame = CFrame.new(9e9,-9e9,9e9)
+                    character.HumanoidRootPart.AssemblyLinearVelocity = Vector3.zero
+                    character.HumanoidRootPart.AssemblyAngularVelocity = Vector3.zero
+                until not dothetech
+                wait(0.1)
+                character.HumanoidRootPart.CFrame=lastcf
+                workspace.Camera.CameraType = Enum.CameraType.Custom
+                workspace.Camera.CameraSubject = character.Humanoid
+                wait(1)
+            end
+        end
+    end
+end)
+coroutine.resume(getgenv().loop)
